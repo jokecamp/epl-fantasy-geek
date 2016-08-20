8d3835c9710cb618e66bc4c5f0de1bc20804b11e
Joe Kampschmidt <jokecamp@gmail.com>
2016-08-20 13:50:14 -0400

    Fix for 16-17 season

--- js/data-svc.js
+++ js/data-svc.js
@@ -1,126 +1,125 @@
+'use strict';
 
 angular.module('myApp.services', [])
-.factory('Data', ['$http', '$filter', function ($http, $filter) {
+    .factory('Data', ['$http', '$filter', '$log', function($http, $filter, $log) {
 
 
-    var getCleanData = function(items, teams, stat) {
+        var getCleanData = function(items, teams) {
 
-      return getTwitterJson().then(function(handles) {
-        return cleanData(items, teams, stat, handles)
-      });
+            return getTwitterJson().then(function(handles) {
+                return cleanData(items, teams, handles);
+            });
 
-    };
+        };
 
-    var cleanData = function(items, teams, stat, handles) {
+        var cleanData = function(items, teams, handles) {
 
-      var players = _.map(items, function(p) {
-        if (p == null) p = {};
+            var players = _.map(items, function(p) {
 
-        var x = {};
-        angular.extend(x, stat);
+                if (p == null) p = {};
 
-        _.forOwn(x, function(value, key) {
-          x[key] = p[value];
-        });
+                // custom data
+                if (p.team > 0) {
+                    p.team = _.find(teams, function(t) {
+                        return t.id === p.team;
+                    });
+                }
 
-        // custom data
-        if (x.team_id > 0) {
-          x.team = _.find(teams, function(t) { return t.id == x.team_id; });
-        }
-        x.position = $filter('pos')(x.element_type_id);
+                p.position = $filter('pos')(p.element_type);
 
-        if (x.position == 'GK') x.posn = 'K';
-        if (x.position == 'FWD') x.posn = 'F';
-        if (x.position == 'MID') x.posn = 'M';
-        if (x.position == 'DEF') x.posn = 'D';
+                if (p.element_type === 1) p.posn = 'K';
+                if (p.element_type == 2) p.posn = 'D';
+                if (p.element_type == 3) p.posn = 'M';
+                if (p.element_type == 4) p.posn = 'F';
 
-
-        x.points_per_cost = x.total_points/ (x.now_cost/10);
-        x.transfer_diff = x.transfers_in_event - x.transfers_out_event;
-        x.pinned = false;
-
-        var handle = _.find(handles, function(h) {
-          return h.id == x.id || ((x.first_name + ' ' + x.second_name) == h.name);
-        });
-        if (handle != null){
-          handle.id = x.id;
-          x.twitter = handle.twitter;
-        }
-
-        return x;
-
-      });
-
-      return _.where(players, function (p) { return p.id > 0; } );
-    };
-
-    var getTeams = function(data) {
-
-      var teams = [];
-      for(var i = 1; i <= 20; i++) {
-        teams.push( data.eiwteams[i]);
-      }
-
-      return teams;
-    };
-
-    var getRoster = function(data) {
-      return data.elements;
-    };
-
-    var getStatColumns = function(data) {
-      return data.elStat;
-    };
-
-    var getPlayers = function(data) {
-      return data.elInfo;
-    };
-
-    var getJson = function(){
-      return $http.get('js/data.json').then(function(res){
-        return res.data;
-      });
-    };
-
-    var getTwitterJson = function(){
-      return $http.get('js/twitter.json').then(function(res){
-        return res.data;
-      });
-    };
+                p.selected_by_percent = parseFloat(p.selected_by_percent);
+                p.points_per_cost = p.total_points / (p.now_cost / 10);
+                p.transfer_diff = p.transfers_in_event - p.transfers_out_event;
+                p.pinned = false;
 
 
-    var getLeagueTable = function(){
-      return $http.get('js/table.json').then(function(res){
-        return res.data;
-      });
-    };
+                p.name = p.first_name + ' ' + p.second_name;
 
-    var getFixtures = function(){
-      // http://api.football-data.org/alpha/teams/66/fixtures
-      return $http.get('js/fixtures.json').then(function(res){
-        return res.data;
-      });
-    };
+                var handle = _.find(handles, function(h) {
+                    return h.id == p.id || ((p.first_name + ' ' + p.second_name) == h.name);
+                });
+                if (handle != null) {
+                    handle.id = p.id;
+                    p.twitter = handle.twitter;
+                }
 
-    var getMeta = function(){
-      // http://api.football-data.org/alpha/teams/66/fixtures
-      return $http.get('js/meta.json').then(function(res){
-        return res.data;
-      });
-    };
+                return p;
 
-    return {
-      getMeta: getMeta,
-      getJson: getJson,
-      getCleanData: getCleanData,
-      players: getPlayers,
-      teams: getTeams,
-      getLeagueTable: getLeagueTable,
-      roster: getRoster,
-      stats: getStatColumns,
-      twitter: function() { return handles; },
-      getTeams: getTeams,
-      getFixtures: getFixtures
-    };
+            });
 
-}]);
+            return _.where(players, function(p) {
+                return p.id > 0;
+            });
+        };
+
+        var getTeams = function(data) {
+            return data.teams;
+        };
+
+        var getRoster = function(data) {
+            return data.elements;
+        };
+
+        var getStatColumns = function(data) {
+            return data.elStat;
+        };
+
+        var getPlayers = function(data) {
+            return data.elements;
+        };
+
+        var getJson = function() {
+            return $http.get('js/static-data.json').then(function(res) {
+                return res.data;
+            });
+        };
+
+        var getTwitterJson = function() {
+            return $http.get('js/twitter.json').then(function(res) {
+                return res.data;
+            });
+        };
+
+
+        var getLeagueTable = function() {
+            return $http.get('js/table.json').then(function(res) {
+                return res.data;
+            });
+        };
+
+        var getFixtures = function() {
+            // http://api.football-data.org/alpha/teams/66/fixtures
+            return $http.get('js/fixtures.json').then(function(res) {
+                return res.data;
+            });
+        };
+
+        var getMeta = function() {
+            // http://api.football-data.org/alpha/teams/66/fixtures
+            return $http.get('js/meta.json').then(function(res) {
+                return res.data;
+            });
+        };
+
+        return {
+            getMeta: getMeta,
+            getJson: getJson,
+            getCleanData: getCleanData,
+            players: getPlayers,
+            teams: getTeams,
+            getLeagueTable: getLeagueTable,
+            roster: getRoster,
+            stats: getStatColumns,
+            twitter: function() {
+                return handles;
+            },
+            getTeams: getTeams,
+            getFixtures: getFixtures
+        };
+
+    }]);
